f957e8bb1f701462dc7597352d43ebd454e73c25
remy <carolinegc07@gmail.com>
2026-07-17 13:40:07 -0500

    conectar la aplicacion con la base de datos real y habilitar usuarios

--- src/routes/routineRoutes.js
+++ src/routes/routineRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { createRoutineHandler, listRoutines } from '../controllers/routineController.js'
+import { createRoutineHandler, listRoutines, listRoutinesByUser, updateRoutineHandler, deleteRoutineHandler } from '../controllers/routineController.js'
 
 const router = Router()
 
 router.get('/', listRoutines)
+router.get('/user/:userId', listRoutinesByUser)
 router.post('/', createRoutineHandler)
+router.put('/:id', updateRoutineHandler)
+router.delete('/:id', deleteRoutineHandler)
 
 export default router
